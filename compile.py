6ce610672af4ddcba948c6bcc12e0f208f0c1755
user <joealsko@gmail.com>
2017-09-13 23:37:25 -0600

    added functions for neg,assign,input,and name tokens; newlines break the code

--- compile.py
+++ compile.py
@@ -195,7 +195,7 @@ def createAssembly(targetFile, flatAst):
 def main():
 	with open(sys.argv[1], "r") as inputFile:
 		reserved = {'print' : 'PRINT', 'input()' : 'INPUT'}
-		tokens = ['INT','PLUS','ASSIGN','PAREN_START','PAREN_END','NEG','ID'] + list(reserved.values())
+		tokens = ['INT','PLUS','ASSIGN','PAREN_START','PAREN_END','NEG','NAME'] + list(reserved.values())
 		t_PRINT = r'print'
 		t_PLUS = r'\+'
 		t_NEG = r'\-'
@@ -204,9 +204,9 @@ def main():
 		t_PAREN_START = r'\('
 		t_PAREN_END = r'\)'
 		#t_VAR = r'[a-zA-Z_][a-zA-Z0-9_]*'
-		def t_ID(t):
+		def t_NAME(t):
 			r'[a-zA-Z_][a-zA-Z_0-9]*'
-			t.type = reserved.get(t.value,'ID')
+			t.type = reserved.get(t.value,'NAME')
 			return t
 		def t_INT(t):
 			r'\d+'
@@ -225,10 +225,10 @@ def main():
 			t.lexer.skip(1)
 		myLexer = lex.lex()
 		myLexer.input(inputFile.read())
-		for token in myLexer:
-			print token
+		#for token in myLexer:
+		#	print token
 		# Parser
-		from compiler.ast import Printnl, Add, Const
+		from compiler.ast import Printnl, Add, Const, UnarySub, Name, Assign, AssName, CallFunc
 		precedence = (
 			('nonassoc','PRINT'),
 			('left','PLUS')
@@ -236,16 +236,28 @@ def main():
 		def p_print_statement(t):
 			'statement : PRINT expression'
 			t[0] = Printnl([t[2]], None)
+		def p_assign_statement(t):
+			'statement : NAME ASSIGN expression'
+			t[0] = Assign([AssName(t[1], 'OP_ASSIGN')], t[3])
 		def p_plus_expression(t):
 			'expression : expression PLUS expression'
 			t[0] = Add((t[1], t[3]))
+		def p_neg_expression(t):
+			'expression : NEG expression'
+			t[0] = UnarySub(t[2]) #{'-', '1'}
 		def p_int_expression(t):
 			'expression : INT'
 			t[0] = Const(t[1])
+		def p_name_expression(t):
+			'expression : NAME'
+			t[0] = Name(t[1])
+		def p_input_expression(t):
+			'expression : INPUT'
+			t[0] = CallFunc(Name('input'), [], None, None)
 		def p_error(t):
 			print "Syntax error at '%s'" % t.value
 		parser = yacc.yacc()
-		#print parser.parse(lexer=myLexer)
+		print parser.parse(lexer=myLexer)
 		'''
 		for token in lexer:
 			print token
