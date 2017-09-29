93ec1d8f11751b7e3f2679adb91719d1abcc0886
user <joealsko@gmail.com>
2017-09-29 09:24:20 -0600

    Removed redundant movl instructions, removed $ from call instruction

--- src/printAssembly.py
+++ src/printAssembly.py
@@ -6,17 +6,22 @@ def createAssembly(targetFile, flatAssem, graph):
 		outputFile.write(".globl main\n")
 		outputFile.write("main:\n")
 		while working != None:
-			outputFile.write(working.operation + " ")
-			if isinstance(working.var1, structs.Var):
-				outputFile.write(str(graph[working.var1.name].color))
+			if isinstance(working.var1, structs.Var) and isinstance(working.var2, structs.Var) and str(graph[working.var1.name].color) == str(graph[working.var2.name].color):
+				working = working.next
 			else:
-				outputFile.write("$" + str(working.var1))
-			if isinstance(working.var2, structs.Var):
-				outputFile.write(", " + str(graph[working.var2.name].color))
-			elif working.var2 != None:
-				outputFile.write(", " + str(working.var2))
-			outputFile.write("\n")
-			working = working.next
+				outputFile.write(working.operation + " ")
+				if isinstance(working.var1, structs.Var):
+					outputFile.write(str(graph[working.var1.name].color))
+				elif working.operation == "call":
+					outputFile.write(str(working.var1))
+				else:
+					outputFile.write("$" + str(working.var1))
+				if isinstance(working.var2, structs.Var):
+					outputFile.write(", " + str(graph[working.var2.name].color))
+				elif working.var2 != None:
+					outputFile.write(", " + str(working.var2))
+				outputFile.write("\n")
+				working = working.next
 		outputFile.write("leave\n")
 		outputFile.write("ret")
 		outputFile.close()
